02a317ad6681e4e7e943b27a7863a477b5549b45
rocket-raccoon <the.alberto.bena@gmail.com>
2014-11-10 02:52:06 -0500

    Navigation panels now light up when hovered

---
+++ mysite/mysite/static/js/general.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    
+    //Remember what the original hover color was
+    var originalColor = "yellow";
+    
+    //Panels light up on home navigator   
+    $(".panel").hover(
+        function() {
+            originalColor = $(this).css("background-color");
+            $( this ).css("background-color", "yellow");
+        }, function() {
+            $( this ).css("background-color", originalColor);
+        }
+    );
+    
+});
+
+
+
+
